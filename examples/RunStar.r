847f72fd7c0835db7132186f4ebfcc68f508cb79
zhezhangsh <zhangz@email.chop.edu>
2015-05-08 14:50:42 -0400

    run star

--- examples/RunStar.r
+++ examples/RunStar.r
@@ -1,8 +1,6 @@
-library(devtools);
-install_github("zhezhangsh/Rnaseq"); # comment out this line if the library has already been installed
-library(Rnaseq);
-
 args<-commandArgs(TRUE); # YAML file name
+
+# read in the .yaml file
 yaml<-yaml.load_file(args[1]);
 
 # Input fastq files
@@ -13,6 +11,7 @@ nm<-names(fastq);
 
 # path to output files
 path<-yaml$output;
+if (!file.exists(path)) dir.create(path, recursive=TRUE);
 
 # other run options
 options<-yaml$options
@@ -23,4 +22,23 @@ if (mode == 1) {
   options$twopassMode<-'None';
 }
 
-cmmd
+# create command line
+cmmd<-sapply(nm, function(nm) {
+  fq1<-fastq[[nm]]$fastq1;
+  fq2<-fastq[[nm]]$fastq2;
+  
+  # Build command line
+  cmmd<-yaml$star;
+  
+  # Input file names
+  if (is.na(fq2) | is.null(fq2)) fn<-fq1 else fn<-paste(fq1, fq2);
+  cmmd<-paste(cmmd, "--readFilesIn", fn);
+  cmmd<-paste(cmmd, "--outFileNamePrefix", paste(path, '/', nm, '_', sep=''));
+  cmmd<-paste(cmmd, paste(paste(paste('--', names(options), sep=''), as.vector(options)), collapse=' '));
+  cmmd;
+})
+
+# Save shell script
+if (is.na(args[2])) fn<-'./RunStar.sh' else fn<-args[2];
+writeLines(paste(cmmd, collapse='\n\n'),  fn);
+
