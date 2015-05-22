a0dfaf4098a9c63d9affcedd48a0026d038be5d2
zhezhangsh <zhangz@email.chop.edu>
2015-05-22 17:39:50 -0400

    star align

---
+++ R/RunStar.r
@@ -0,0 +1,61 @@
+RunStar<-function(fn.yaml, execute=FALSE) {
+  # fn.yaml   Name of the .yaml file defines the run inputs
+  
+  library(yaml);
+  yaml<-yaml.load_file(fn.yaml);
+  
+  # Input fastq files
+  fastq<-yaml$fastq;
+  
+  # Sample names, used as prefix of output files
+  nm<-names(fastq);
+  
+  # path to output files
+  path<-yaml$output;
+  if (!file.exists(path)) dir.create(path, recursive=TRUE);
+  
+  # other run options
+  options<-yaml$options
+  options$twopassMode<-'None'; # Turn off the persample 2-pass mapping mode
+  
+  # Pre-defined runnign mode requiring specific options
+  n<-as.integer(yaml$pass[1]);
+  if (n<1) n<-1;
+  path.pass<-paste(path, '/pass_', 1:n, sep='');
+  path.pass<-gsub('//', '/', path.pass);
+  sapply(path.pass, function(path) if (!file.exists(path)) dir.create(path))->x; 
+  
+  # create command lines
+  cmmd<-lapply(1:n, function(i) {
+    lines<-sapply(nm, function(nm) {
+      ln<-c('############', paste('#', nm), yaml$star);
+   
+      # Add fastq file(s)
+      if (is.na(fastq[[nm]]$fastq2) | is.null(fastq[[nm]]$fastq2)) fn<-fastq[[nm]]$fastq1 else fn<-paste(fastq[[nm]]$fastq1, fastq[[nm]]$fastq2);
+      ln<-c(ln, paste("--readFilesIn", fn));
+      
+      # Add other options
+      ln<-c(ln, as.vector(sapply(names(options), function(op) paste('--', op, ' ', as.vector(options[[op]]), sep=''))));
+      
+      # Add output path and prefix
+      ln<-c(ln, paste("--outFileNamePrefix ", path.pass[i], '/', nm, '_', sep=''));
+      
+      # Add SJ files created by the last pass
+      if (i > 1) {
+        fn.sj<-dir(path.pass[i-1]);
+        fn.sj<-paste(path.pass[i-1], fn.sj[grep('SJ.out.tab$', fn.sj)], sep='/');
+        ln<-c(ln, paste(c("--sjdbFileChrStartEnd", fn.sj), collapse=' '));
+      }
+      
+      if (execute) system(paste(ln[-(1:2)], collapse=' '));
+      
+      c(ln, '', '');
+    });
+    
+    writeLines(paste(unlist(lines, use.names=FALSE), '\\'), paste(path.pass[i], 'RunStar.sh', sep='/'));
+  });
+  
+  names(cmmd)<-paste('pass', 1:length(cmmd), sep='');
+  
+  cmmd;
+}
--- examples/RunStar.r
+++ examples/RunStar.r
@@ -19,11 +19,11 @@ options<-yaml$options
 
 # Pre-defined runnign mode requiring specific options
 mode<-yaml$mode;
-if (mode == 1) {
+if (mode == 1 | mode == 2) { # run the 2nd pass explicitly
   options$twopassMode<-'None';
-}
+} 
 
-# create command line
+# create command line for pass 1
 cmmd<-sapply(nm, function(nm) {
   fq1<-fastq[[nm]]$fastq1;
   fq2<-fastq[[nm]]$fastq2;
@@ -37,7 +37,30 @@ cmmd<-sapply(nm, function(nm) {
   cmmd<-paste(cmmd, "--outFileNamePrefix", paste(path, '/', nm, '_', sep=''));
   cmmd<-paste(cmmd, paste(paste(paste('--', names(options), sep=''), as.vector(options)), collapse=' '));
   cmmd;
-})
+});
+
+# create command lines for pass 2
+if(mode==2) {
+  fn.sj<-paste(path, dir(yaml$output), sep='/');
+  fn.sj<-fn.sj[grep('_SJ.out.tab$', fn.sj)];
+  cmmd2<-sapply(nm, function(nm) {
+    fq1<-fastq[[nm]]$fastq1;
+    fq2<-fastq[[nm]]$fastq2;
+    
+    # Build command line
+    cmmd<-yaml$star;
+    
+    # Input file names
+    if (is.na(fq2) | is.null(fq2)) fn<-fq1 else fn<-paste(fq1, fq2);
+    cmmd<-paste(cmmd, "--readFilesIn", fn);
+    cmmd<-paste(cmmd, "--outFileNamePrefix", paste(path, '/pass2/', nm, '_', sep=''));
+    cmmd<-paste(cmmd, paste(paste(paste('--', names(options), sep=''), as.vector(options)), collapse=' '));
+    cmmd<-paste(cmmd, '--sjdbFileChrStartEnd', paste(fn.sj, collapse=' '));
+    cmmd;
+  });
+  
+  cmmd<-c(cmmd, '', '', cmmd2);
+}
 
 # Save shell script
 if (is.na(args[2])) fn<-'./RunStar.sh' else fn<-args[2];
