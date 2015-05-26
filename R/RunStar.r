f52fc5466130988a4b522dbc7805731f00cefe0c
zhezhangsh <zhangz@email.chop.edu>
2015-05-26 12:12:17 -0400

    Run STAR

--- R/RunStar.r
+++ R/RunStar.r
@@ -1,5 +1,7 @@
+# Run STAR based on inputs defined by a .yaml file
 RunStar<-function(fn.yaml, execute=FALSE) {
   # fn.yaml   Name of the .yaml file defines the run inputs
+  # execute   Just save the shell script if FALSE; run STAR alignment if TRUE
   
   library(yaml);
   yaml<-yaml.load_file(fn.yaml);
@@ -45,19 +47,63 @@ RunStar<-function(fn.yaml, execute=FALSE) {
         fn.sj<-paste(path.pass[i-1], '/', nms, '_SJ.out.tab', sep='');
         ln<-c(ln, paste(c("--sjdbFileChrStartEnd", fn.sj), collapse=' '));
       }
-      
-      if (execute) system(paste(ln[-(1:2)], collapse=' '));
-      
+            
       c(ln, '', '');
     });
     
     writeLines(paste(unlist(lines, use.names=FALSE), '\\'), paste(path.pass[i], 'RunStar.sh', sep='/'));
     
+    names(lines)<-
     lines;
   });
   
   names(cmmd)<-paste('pass', 1:length(cmmd), sep='');
   cmmd<-lapply(cmmd, function(cmmd) sapply(cmmd, function(cmmd) paste(cmmd[-(1:2)], collapse=' ')));
+  
+  ##########################################################################################################
+  ##########################################################################################################
+  # Make the actual runs
+  if (execute) {
+    # Keep track of process
+    fn.log<-paste(path, 'log.rds', sep='/'); 
+    if (!file.exists(fn.log)) {
+      log<-lapply(1:length(cmmd), function(i) rep(FALSE, length(nms)));
+      saveRDS(log, file=fn.log); 
+    } else log<-readRDS(fn.log);
+    
+    for (i in 1:length(log)) { # For each pass
+      for (j in 1:length(nms)) { # For each sample
+        c<-cmmd[[i]][[j]]; # The command to run
+        
+        fn.req<-unlist(c(yaml$star, yaml$output, yaml$options$genomeDir, yaml$fastq[[j]]));
+        fn.req<-fn.req[!file.exists(fn.req)];
+        if (i > 1) fn.sj<-paste(path.pass[i-1], '/', nms, '_SJ.out.tab', sep='') else fn.sj<-character();
+        fn.sj<-fn.sj[!file.exists(fn.sj)];
+        fn.req<-c(fn.req, fn.sj);
+        if (length(fn.req) > 0) {
+          w<-warning("Required file(s) not exist: \n", paste(fn.req, collapse='\n'));
+          cat(c('\n', '\n', date(), '\n', w), file=paste(yaml$output, 'RunStar.log', sep='/'), append=TRUE);
+        } else {
+          ##########################################################################################
+          cd<-system(c, intern=FALSE, ignore.stdout=TRUE, ignore.stderr=TRUE, wait=TRUE); # Run STAR
+          ##########################################################################################
+          
+          # Run succeeded
+          if (cd == 0) {
+            log[[i]][[j]]<-TRUE;
+            saveRDS(log, file=fn.log);
+            cat(c('\n', '\n', date(), '\n', 'Finished alignment:', nms[j], 'pass', i, '\n'), file=paste(yaml$output, 'RunStar.log', sep='/'));
+          }
+        }
+      }
+      # Delete intermediate SAM files
+      if (i < length(log)) {
+        fn.sam<-dir(path.pass[i]);
+        fn.sam<-fn.sam[grep('Aligned.out.sam', fn.sam)];
+        file.remove(paste(path.pass[i], fn.sam, sep='/'));
+      }
+    } # End of for (i in 1:length(log))
+  }
   
   cmmd;
 }
