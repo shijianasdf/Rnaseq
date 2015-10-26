2934a6f88401c5d0bbf377708f37a95a83d83a45
zhezhangsh <zhangz@email.chop.edu>
2015-10-26 17:38:27 -0400

    gapped align

--- R/MapGA.R
+++ R/MapGA.R
@@ -62,11 +62,11 @@ MapInterval2Exon<-function(intv, exons, type='within', strand=0, ex2tx=c(), tx2g
   if (strand == 0) ig<-TRUE else ig<-FALSE;
   
   if (strand < 0) strand(exons)<-c('+'='-', '-'='+', '*'='*')[as.vector(strand(exons))];
-  x<-GenomicAlignments::findOverlaps(intv, exons, type=type, ignore.strand=ig);
-  print(class(x)); print(2);
+  x<-SummarizedExperiment::findOverlaps(intv, exons, type=type, ignore.strand=ig);
+  print(class(x)); print(1);
   saveRDS(x, '~/tmp0.rds');
   olap<-as.matrix(x);
-  
+  print(3);
   mp<-intv[olap[, 1]];
   ex<-exons[olap[, 2]];
   mp$exon<-names(ex);
