19b30320d48f45c6487b408d159e801aefa96e54
zhezhangsh <zhangz@email.chop.edu>
2015-10-26 15:28:00 -0400

    gapped align

--- R/MapGA.R
+++ R/MapGA.R
@@ -1,11 +1,10 @@
-LoadGa<-function(bam, gr=NA, paired=TRUE, exons=NA, ex2tx=c(), tx2gn=c(), max.cluster=1, min.mapq=1, strand.match=-1) {
+LoadGa<-function(bam, gr=NA, paired=TRUE, exons=NA, ex2tx=c(), tx2gn=c(), min.mapq=1, strand.match=0) {
   # bam             Full path to bam file
   # gr              GRanges object specifies regions or chromsomes; the whole bam file if NA
   # paired          Paired or single ended reads
   # exons           Location of exons
   # ex2tx           Exon to transcript mapping
   # tx2gn           Transcript to gene mapping
-  # max.cluster     Max number of clusters for parrallele processing
   # strand.match    Read-to-exon strand match; 0 no match; 1 match; -1 reverse match. Should be reverse match for most RNA-seq protocols  
   
   require("GenomicRanges");
@@ -14,12 +13,11 @@ LoadGa<-function(bam, gr=NA, paired=TRUE, exons=NA, ex2tx=c(), tx2gn=c(), max.cl
   
   if (identical(gr, NA)) {
     chr<-scanBamHeader(bam)[[1]][[1]];
-    gr<-lapply(names(chr), function(nm) GRanges(nm, IRanges(1, chr[nm])));
-    names(gr)<-names(chr);
-  } else {
-    gr<-lapply(unique(as.vector(seqnames(gr))), function(c) gr[seqnames(gr)==c]);
-    names(gr)<-sapply(gr, function(gr) as.vector(seqnames(gr))[1]);
-  }
+    gr<-GRanges(names(chr), IRanges(1, chr));
+  } 
+    
+  gr<-lapply(unique(as.vector(seqnames(gr))), function(c) gr[seqnames(gr)==c]);
+  names(gr)<-sapply(gr, function(gr) as.vector(seqnames(gr))[1]);
   
   #ncore<-min(max.cluster, length(gr));
   
@@ -42,6 +40,7 @@ LoadGa<-function(bam, gr=NA, paired=TRUE, exons=NA, ex2tx=c(), tx2gn=c(), max.cl
         }
       }
     }
+    rd$dumped<-MapInterval2Exon(ConvertGa2Gr(rd$dumped), exons, 'within', 0, ex2tx, tx2gn);
     
     rd; 
   });
@@ -51,7 +50,7 @@ LoadGa<-function(bam, gr=NA, paired=TRUE, exons=NA, ex2tx=c(), tx2gn=c(), max.cl
 }
 
 # Maping mapped read intervals to exons or other ungapped genomic features
-MapInterval2Exon<-function(intv, exons, type='within', strand=1, ex2tx=c(), tx2gn=c()) {
+MapInterval2Exon<-function(intv, exons, type='within', strand=0, ex2tx=c(), tx2gn=c()) {
   # intv    GRanges object, genomic intervals returned by the ConvertGa2Gr function
   # exons   GRanges object, location of exons or other ungapped genomic features; must have unique names
   # type    Type of overlapping, by default, mapping interval must be completely within exon
@@ -72,15 +71,18 @@ MapInterval2Exon<-function(intv, exons, type='within', strand=1, ex2tx=c(), tx2g
   
   mx<-rep(0, length(mp)); # max extension allowed by the boundary of exon
   ext<-mp$extension;
-  str<-as.vector(strand(mp));
+  str0<-as.vector(strand(mp));
   stt0<-start(mp);
   end0<-end(mp);
+  str1<-as.vector(strand(ex));
   stt1<-start(ex);
   end1<-end(ex);
-  ind<-which(str=='-' & ext>0);
+  ind<-which(str0=='-' & ext>0);
   if (length(ind)>0) mx[ind] <- pmin(stt0[ind]-stt1[ind], ext[ind]);
-  ind<-which(str=='+' & ext>0);
+  ind<-which(str0=='+' & ext>0);
   if (length(ind)>0) mx[ind] <- pmin(end1[ind]-end0[ind], ext[ind]);
+  mp$same.strand<-str0==str1;
+  if (strand==-1) mp$same.strand<-!mp$same.strand;
   mp$max.extension<-mx;
   
   if (length(ex2tx)) mp$transcript<-as.vector(ex2tx[mp$exon]);
@@ -90,12 +92,12 @@ MapInterval2Exon<-function(intv, exons, type='within', strand=1, ex2tx=c(), tx2g
 }
 
 # Load BAM file of paired end reads
-LoadGaPe<-function(bam, gr, min.mapq=1) {
+LoadGaPe<-function(bam, gr, min.mapq=1, wht=character(0)) {
   
   require("GenomicRanges");
   require("GenomicAlignments");
   
-  prm<-ScanBamParam(which=gr, mapqFilter=min.mapq, flag=scanBamFlag(
+  prm<-ScanBamParam(which=gr, what=wht, mapqFilter=min.mapq, flag=scanBamFlag(
     isPaired=TRUE,
     isUnmappedQuery=FALSE,
     hasUnmappedMate=FALSE,
@@ -108,18 +110,19 @@ LoadGaPe<-function(bam, gr, min.mapq=1) {
   gr.fst<-ConvertGa2Gr(reads@first);
   gr.lst<-ConvertGa2Gr(reads@last);
   
+  
   loaded<-list(names=reads@NAMES, paired=TRUE, interval=list(first=gr.fst, last=gr.lst), dumped=dmp);
   
   loaded;
 }
 
 # Load BAM file of single end reads
-LoadGaSe<-function(bam, gr, min.mapq=1) {
+LoadGaSe<-function(bam, gr, min.mapq=1, wht=character(0)) {
   
   require("GenomicRanges");
   require("GenomicAlignments");
   
-  prm<-ScanBamParam(which=gr, mapqFilter=min.mapq, flag=scanBamFlag(
+  prm<-ScanBamParam(which=gr, what=wht, mapqFilter=min.mapq, flag=scanBamFlag(
     isUnmappedQuery=FALSE,
     isNotPassingQualityControls=FALSE));
   
