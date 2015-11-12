a72725e3a10bfd8562099ce2f98f9eb5cb661fa1
zhezhangsh <zhangz@email.chop.edu>
2015-11-12 14:42:55 -0500

    count read

--- R/CountRead.R
+++ R/CountRead.R
@@ -1,16 +1,28 @@
 CountRead<-function(ga.list, feature=c('gene'), ncl=1, match.strand=0) {
-  # ga.list     Gapped alignment of reads loaded by the Rnaseq::LoadGa function, split by chromosomes
+  # ga.list     File name (.rds) or loaded object of gapped alignment of reads loaded by the Rnaseq::LoadGa function, split by chromosomes
   # feature     Genomic feature to make the read count, such as 'exon', 'gene'
   
   require("GenomicRanges");
   require("GenomicAlignments");
   require("Rnaseq");
   
+  if (class('ga.list') == 'character') {
+    cat('Loading processed data of gapped alignment\n'); 
+    ga.list<-readRDS(ga.list); 
+  }
+  
   feature<-tolower(feature);
   
-  if ('gene' %in% feature) CountGeneRead(ga.list, ncl);
+  ct<-list();
+  if ('gene' %in% feature) {
+    cat('Calculating gene-level read count\n')
+    ct$Gene<-GetGeneCount(ga.list, ncl, match.strand);
+  }
+  
+  ct;
 }
 
+# One-on-one mapping between reads and features
 GetUniqueMapping<-function(gr, read.name, feature, match.strand=0) {
   
   require("GenomicRanges");
@@ -28,6 +40,7 @@ GetUniqueMapping<-function(gr, read.name, feature, match.strand=0) {
   }
 }
 
+# map reads to a feature which corresponds to a column in the elementMetadata
 MapRead2Feature<-function(ga.list, feature, ncl=1, match.strand=0) {
   
   require("GenomicRanges");
@@ -38,12 +51,12 @@ MapRead2Feature<-function(ga.list, feature, ncl=1, match.strand=0) {
   
   ncl<-min(ncl, length(ga.list));
   
-  if (paired) {
-    mp.fst<-parallel::mclapply(ga.list, function(ga) GetUniqueMapping(ga[[3]][[1]], ga[[1]], feature, match.strand), mc.cores = ncl);
+  if (paired) { # PE
+    mp.fst<-parallel::mclapply(ga.list, function(ga) GetUniqueMapping(ga[[3]][[1]], ga[[1]], feature,    match.strand), mc.cores = ncl);
     mp.lst<-parallel::mclapply(ga.list, function(ga) GetUniqueMapping(ga[[3]][[2]], ga[[1]], feature, -1*match.strand), mc.cores = ncl);
     mp<-parallel::mclapply(list(mp.fst, mp.lst), function(lst) do.call('rbind', lst));
     mp<-parallel::mclapply(mp, function(mp) lapply(split(mp[, 1], mp[, 2]), unique));
-  } else {
+  } else { # SE
     mp<-parallel::mclapply(ga.list, function(ga) GetUniqueMapping(ga[[3]], ga[[1]], feature, match.strand), mc.cores = ncl);
     mp<-do.call('rbind', mp);
     mp<-lapply(split(mp[, 1], mp[, 2]), unique);
@@ -52,10 +65,23 @@ MapRead2Feature<-function(ga.list, feature, ncl=1, match.strand=0) {
   mp;
 }
 
+# split unpaired/paired reads
+SplitPairUnpaired<-function(mapped1, mapped2) {
+  # split paired end reads and unpaired ones
+  gid<-sort(unique(c(names(mapped1), names(mapped2))));
+  pid<-cbind(mapped1[gid], mapped2[gid]);
+  apply(pid, 1, function(ids) {
+    id<-ids[[1]]; 
+    id0<-id[id %in% ids[[2]]]; 
+    id1<-c(ids[[1]], ids[[2]]); 
+    id1<-id1[!(id1 %in% id0)]; 
+    list(paired=id0, unpaired=id1);
+  });
+}
+
+# Gene level read count
 GetGeneCount<-function(ga.list, ncl=1, match.strand=0) {
-  mapped.same<-MapRead2Feature(ga.list, 'gene', ncl, 1); 
-  mapped.rev<-MapRead2Feature(ga.list, 'gene', ncl, -1); 
-  
+
   paired<-ga.list[[1]]$paired;
   
   ##############################################################
@@ -64,21 +90,110 @@ GetGeneCount<-function(ga.list, ncl=1, match.strand=0) {
     ##############################################################
     # strand specific cDNA library
     if (match.strand !=0) { # strand-specific mapping
-      if (match.strand > 0) { # cDNA library matching sense strand
-        mapped<-MapRead2Feature(ga.list, 'gene', ncl, 1);  # mapped to sense strand
-        mapped.anti<-MapRead2Feature(ga.list, 'gene', ncl, -1);  # mapped to antisense strand
-      } else { # cDNA library matching anti-sense strand
-        mapped<-MapRead2Feature(ga.list, 'gene', ncl, -1);  # mapped to sense strand
-        mapped.anti<-MapRead2Feature(ga.list, 'gene', ncl, 1);  # mapped to antisense strand
-      }
+      cat('Mapping reads to genes\n');
+      mapped<-MapRead2Feature(ga.list, 'gene', ncl, match.strand);  # mapped to sense strand
+      mapped.anti<-MapRead2Feature(ga.list, 'gene', ncl, -1*match.strand);  # mapped to antisense strand
       
+      splt<-SplitPairUnpaired(mapped[[1]], mapped[[2]]);
+      mp.paired<-lapply(splt, function(mp) mp[[1]]); 
+      mp.unpaired<-lapply(splt, function(mp) mp[[2]]);
       
+      claimed<-c(); # reads have been assigned to a category
+      ###############################################
+      ###############################################      
+      rds<-unlist(mp.paired, use.names=FALSE);
+      gns<-rep(names(mp.paired), sapply(mp.paired, length));
+      dup<-rds %in% rds[duplicated(rds)]; 
+    
+      ############################################  
+      paired.unique<-split(rds[!dup], gns[!dup]); # both ends of a pair mapped to one and only one gene
+      ############################################  
+      
+      ############################################  
+      paired.multiple<-split(rds[dup], gns[dup]); # both ends of a pair mapped to multiple genes
+      ############################################  
+
+      claimed<-c(claimed, rds);
+      ################################################
+      ################################################      
+      # reads mapped to antisense strand
+      splt<-SplitPairUnpaired(mapped.anti[[1]], mapped.anti[[2]]);
+      anti.paired<-lapply(splt, function(mp) mp[[1]]);
+      anti.unpaired<-lapply(splt, function(mp) mp[[2]]);
+      
+      rds<-unlist(anti.paired, use.names=FALSE);
+      gns<-rep(names(anti.paired), sapply(anti.paired, length));
+      cld<-rds %in% claimed;
+      rds<-rds[!cld];
+      gns<-gns[!cld];
+      dup<-rds %in% rds[duplicated(rds)]; 
+      
+      ############################################  
+      paired.unique.anti<-split(rds[!dup], gns[!dup]); # both paired mapped to the antisense of one and only one gene
+      ############################################  
+      
+      ############################################  
+      paired.multiple.anti<-split(rds[dup], gns[dup]); # both paired mapped to the antisense of one and only one gene
+      ############################################  
+      
+      claimed<-c(claimed, rds);
+      ################################################
+      ################################################      
+      rds<-unlist(mp.unpaired, use.names=FALSE);
+      gns<-rep(names(mp.unpaired), sapply(mp.unpaired, length));
+      cld<-rds %in% claimed;
+      rds<-rds[!cld];
+      gns<-gns[!cld];
+      dup<-rds %in% rds[duplicated(rds)];  
+      
+      ############################################  
+      unpaired.unique<-split(rds[!dup], gns[!dup]); # unpaired reads mapped to one and only one gene
+      ############################################  
+      
+      ############################################  
+      unpaired.multiple<-split(rds[dup], gns[dup]); # unpaired reads mapped to multiple genes
+      ############################################  
+      
+      claimed<-c(claimed, rds);
+      ################################################
+      ###############################################      
+      rds<-unlist(anti.unpaired, use.names=FALSE);
+      gns<-rep(names(anti.unpaired), sapply(anti.unpaired, length));
+      cld<-rds %in% claimed;
+      rds<-rds[!cld];
+      gns<-gns[!cld];
+      dup<-rds %in% rds[duplicated(rds)]; 
+      
+      ############################################  
+      unpaired.unique.anti<-split(rds[!dup], gns[!dup]); # unpaired reads mapped to the antisense of one and only one gene
+      ############################################  
+      
+      ############################################  
+      unpaired.multiple.anti<-split(rds[dup], gns[dup]); # unpaired reads mapped to the antisense of one and only one gene
+      ############################################  
+      
+      claimed<-c(claimed, rds);
+      
+      grps<-list(paired.unique, paired.multiple, unpaired.unique, unpaired.multiple, paired.unique.anti, paired.multiple.anti, unpaired.unique.anti, unpaired.multiple.anti); 
+      names(grps)<-c('unique', 'multiple', 'unpaired_unique', 'unpaired_multiple', 
+                     'unique_antisense', 'multiple_antisense', 'unpaired_unique_antisense', 'unpaired_multiple_antisense');
     } else { 
       ##############################################################
       # strand non-specific cDNA library  
-      mapped.same<-MapRead2Feature(ga.list, 'gene', ncl, 1);  # mapped to sense strand
-      mapped.rev<-MapRead2Feature(ga.list, 'gene', ncl, -1);  # mapped to antisense strand
-      
+      # TODO
+      grps<-list();
     }
+  } else { # unpaired reads
+    # TODO
+    grps<-list();
   }
+  
+  # Count reads
+  ct<-lapply(grps, function(gp) sapply(gp, length));
+  gid<-sort(unique(unlist(lapply(ct, names), use.names=FALSE)));
+  c<-matrix(0, nr=length(gid), nc=length(grps), dimnames = list(gid, names(grps)));
+  for (i in 1:length(grps)) c[names(ct[[i]]), i]<-ct[[i]];
+  
+  list(count=c, mapping=grps);
 }
+
