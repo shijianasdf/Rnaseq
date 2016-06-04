f007ad809e7df0eafac79ad263f5e95b023e6a26
Zhang <zhangz@email.chop.edu>
2016-06-04 02:27:07 -0400

    normalize

--- R/NormalizeRNAseq.R
+++ R/NormalizeRNAseq.R
@@ -4,23 +4,21 @@ NormalizeRNAseq <- function(cnt, len=NA, methods=c("NormTotalCount", "NormMedian
   
   norm <- list(Original=cnt); 
   
-  if ("NormTotalCount" %in% methods) norm$Total_Count <- NormTotalCount(cnt); 
-  if ("NormMedian" %in% methods) norm$Median <- NormMedian(cnt); 
-  if ("NormQQ" %in% methods) norm$Quantile_Quantile <- NormMedian(cnt, ref='median'); 
-  if ("NormUpperQuantile" %in% methods) norm$Upper_Quantile <- NormUpperQuantile(cnt); 
-  if ("NormRLE" %in% methods) norm$edgeR_RLE <- NormRLE(cnt); 
-  if ("NormTMM" %in% methods) norm$edgeR_TMM <- NormTMM(cnt); 
-  if ("NormDESeq" %in% methods) norm$DESeq <- NormDESeq(cnt); 
+  if ("NormTotalCount" %in% methods) norm$Total_Count <- round(NormTotalCount(cnt)); 
+  if ("NormMedian" %in% methods) norm$Median <- round(NormMedian(cnt)); 
+  if ("NormQQ" %in% methods) norm$Quantile_Quantile <- round(NormMedian(cnt, ref='median')); 
+  if ("NormUpperQuantile" %in% methods) norm$Upper_Quantile <- round(NormUpperQuantile(cnt)); 
+  if ("NormRLE" %in% methods) norm$edgeR_RLE <- round(NormRLE(cnt)); 
+  if ("NormTMM" %in% methods) norm$edgeR_TMM <- round(NormTMM(cnt)); 
+  if ("NormDESeq" %in% methods) norm$DESeq <- round(NormDESeq(cnt)); 
   if ("NormFPKM" %in% methods & length(len)==nrow(cnt)) norm$FPKM <- NormFPKM(cnt, len); 
   if ("NormTPM" %in% methods & length(len)==nrow(cnt)) norm$TPM <- NormTPM(cnt, len); 
   if ("NormLoess" %in% methods) {
     d <- log(cnt+1); 
     d <- NormLoess(d); 
-    d[d<0] <- 0; 
-    norm$Loess <- exp(d);
+    d[d<1] <- 1; 
+    norm$Loess <- round(exp(d)-1);
   }
-  
-  norm <- lapply(norm, round); 
   
   norm; 
 }
