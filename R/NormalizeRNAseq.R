2f7f8fb7592fe248888f01fb1dddbb00e6f2d8a8
Zhang <zhangz@email.chop.edu>
2016-06-23 23:56:02 -0400

    normalization

--- R/NormalizeRNAseq.R
+++ R/NormalizeRNAseq.R
@@ -14,17 +14,17 @@ NormalizeRNAseq <- function(cnt, len=NA,  methods=c("NormTotalCount", "NormMedia
   
   norm <- list(Original=cnt); 
   
-  if ("NormTotalCount" %in% methods) norm$Total_Count <- round(NormTotalCount(cnt)); 
-  if ("NormMedian" %in% methods) norm$Median <- round(NormMedian(cnt)); 
-  if ("NormQQ" %in% methods) norm$Quantile_Quantile <- round(NormQQ(cnt, ref='median')); 
-  if ("NormUpperQuantile" %in% methods) norm$Upper_Quantile <- round(NormUpperQuantile(cnt)); 
-  if ("NormTMM" %in% methods) norm$Trimmed_Mean <- round(NormTMM(cnt)); 
-  if ("NormRLE" %in% methods) norm$Relative_Log <- round(NormRLE(cnt)); 
-  if ("NormDESeq" %in% methods) norm$DESeq <- round(NormDESeq(cnt)); 
+  if ("NormTotalCount" %in% methods) norm$Total_Count             <- NormTotalCount(cnt); 
+  if ("NormMedian" %in% methods) norm$Median                      <- NormMedian(cnt); 
+  if ("NormQQ" %in% methods) norm$Quantile_Quantile               <- NormQQ(cnt, ref='median'); 
+  if ("NormUpperQuantile" %in% methods) norm$Upper_Quantile       <- NormUpperQuantile(cnt); 
+  if ("NormTMM" %in% methods) norm$Trimmed_Mean                   <- NormTMM(cnt); 
+  if ("NormRLE" %in% methods) norm$Relative_Log                   <- NormRLE(cnt); 
+  if ("NormDESeq" %in% methods) norm$DESeq                        <- NormDESeq(cnt); 
   if ("NormFPKM" %in% methods & length(len)==nrow(cnt)) norm$FPKM <- NormFPKM(cnt, len); 
-  if ("NormTPM" %in% methods & length(len)==nrow(cnt)) norm$TPM <- NormTPM(cnt, len); 
-  if ("NormLoess" %in% methods) norm$Loess <- normLog('NormLoess', cnt); 
-  if ("NormCyclicLoess" %in% methods) norm$Cyclic_Loess <- normLog('NormCyclicLoess', cnt); 
+  if ("NormTPM" %in% methods & length(len)==nrow(cnt)) norm$TPM   <- NormTPM(cnt, len); 
+  if ("NormLoess" %in% methods) norm$Loess                        <- normLog('NormLoess', cnt); 
+  if ("NormCyclicLoess" %in% methods) norm$Cyclic_Loess           <- normLog('NormCyclicLoess', cnt); 
 
   norm; 
 }
