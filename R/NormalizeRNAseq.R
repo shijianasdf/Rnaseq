fbc7dd251d7dfe2a8048af65043ef7f5da980e9a
Zhang <zhangz@email.chop.edu>
2016-06-23 23:40:56 -0400

    normalization

--- R/NormalizeRNAseq.R
+++ R/NormalizeRNAseq.R
@@ -1,23 +1,30 @@
 # Normalize a read count matrix from RNA-seq
-NormalizeRNAseq <- function(cnt, len=NA, methods=c("NormTotalCount", "NormMedian", "NormQQ", "NormUpperQuantile", "NormRLE", 
+NormalizeRNAseq <- function(cnt, len=NA,  methods=c("NormTotalCount", "NormMedian", "NormQQ", "NormUpperQuantile", "NormRLE", 
                                                    "NormTMM", "NormDESeq", "NormLoess", "NormFPKM", "NormTPM")) {
+  
+  normLog <- function(mthd, mtrx) {
+    sz <- sum(rowMeans(mtrx+1)/10^6); 
+    d <- log2((mtrx+0.5)/sz); 
+    d <- do.call(mthd, list(mtrx=d)); 
+    d <- exp(d*log(2)); 
+    d <- d*sz-0.5;
+    d[d<0] <- 0;
+    d
+  }; 
   
   norm <- list(Original=cnt); 
   
   if ("NormTotalCount" %in% methods) norm$Total_Count <- round(NormTotalCount(cnt)); 
   if ("NormMedian" %in% methods) norm$Median <- round(NormMedian(cnt)); 
-if ("NormQQ" %in% methods) norm$Quantile_Quantile <- round(NormQQ(cnt, ref='median')); 
+  if ("NormQQ" %in% methods) norm$Quantile_Quantile <- round(NormQQ(cnt, ref='median')); 
   if ("NormUpperQuantile" %in% methods) norm$Upper_Quantile <- round(NormUpperQuantile(cnt)); 
   if ("NormTMM" %in% methods) norm$Trimmed_Mean <- round(NormTMM(cnt)); 
+  if ("NormRLE" %in% methods) norm$Relative_Log <- round(NormRLE(cnt)); 
   if ("NormDESeq" %in% methods) norm$DESeq <- round(NormDESeq(cnt)); 
   if ("NormFPKM" %in% methods & length(len)==nrow(cnt)) norm$FPKM <- NormFPKM(cnt, len); 
   if ("NormTPM" %in% methods & length(len)==nrow(cnt)) norm$TPM <- NormTPM(cnt, len); 
-  if ("NormLoess" %in% methods) {
-    d <- log(cnt+1); 
-    d <- exp(NormLoess(d))-1; 
-    d[d<0] <- 0;
-    norm$Loess <- round(d);
-  }
-  
+  if ("NormLoess" %in% methods) norm$Loess <- normLog('NormLoess', cnt); 
+  if ("NormCyclicLoess" %in% methods) norm$Cyclic_Loess <- normLog('NormCyclicLoess', cnt); 
+
   norm; 
 }
